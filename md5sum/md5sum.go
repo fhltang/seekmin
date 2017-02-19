945868aeb740d663d648523a8ae2e33f53a7fdcf
Francis Tang <francis.tang@gmail.com>
2017-02-18 21:40:21 -0800

    Add pprof export to md5sum.go.

--- md5sum/md5sum.go
+++ md5sum/md5sum.go
@@ -5,11 +5,34 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"log"
+	"net/http"
+	_ "net/http/pprof"
 	"os"
+	"sync"
 )
+
+var port = flag.Int(
+	"port", 0,
+	"Port on which to run an HTTP server for debug stats.")
+var exitOnCompletion = flag.Bool(
+	"exit_on_completion", false,
+	"Exit on completion.  If --port is specified, setting this to false "+
+		"will cause the program to continue listening for HTTP "+
+		"requests.")
 
 func main() {
 	flag.Parse()
+
+	var server_exited sync.WaitGroup
+	if *port != 0 {
+		log.Printf("Starting server on port %d", *port)
+		go func() {
+			server_exited.Add(1)
+			log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), nil))
+			server_exited.Done()
+		}()
+	}
 
 	for _, file := range flag.Args() {
 		f, err := os.Open(file)
@@ -27,5 +50,9 @@ func main() {
 			}
 			fmt.Printf("%x  %s\n", hash.Sum(nil), file)
 		}()
+	}
+
+	if !*exitOnCompletion {
+		server_exited.Wait()
 	}
 }
